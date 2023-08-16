69941c5f4e6c73d8a23c549d262062242e926173
RedAnonymousITA2021 <78427215+RedAnonymousITA@users.noreply.github.com>
2023-08-16 17:46:32 +0200

    new style

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function dow(){
+    window.location.href=('download.html');
+}
+function github(){
+    window.location.href = ('https://github.com/RedAnonymousITA');
+}
+function yt_dow(){
+    window.location.href=('https://github.com/RedAnonymousITA/YouTube_Download/releases/download/v0.1/Youtube-DWN.Windows.zip');
+}
+function home(){
+    window.location.href=('index.html');
+}
