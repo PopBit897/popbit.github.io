b0aeba024931c5988ea592cb1b496f7562303f61
RedAnonymousITA2021 <78427215+RedAnonymousITA@users.noreply.github.com>
2023-10-29 09:22:24 +0100

    new style

---
+++ js/button_download.js
@@ -0,0 +1,27 @@
+function button_sel(response){
+    switch(response){
+        case 1:
+            window.location.href='https://github.com/RedAnonymousITA/YouTube_Download/releases/download/v0.1/Youtube-DWN.Windows.zip';
+            break;
+        case 2:
+            window.location.href='https://github.com/RedAnonymousITA/cimpx/archive/refs/tags/test-Alpha0.0.3.zip';
+            break;
+        case 3:
+            window.location.href='https://github.com/RedAnonymousITA/ip_tools_GUI/archive/refs/tags/v0.0.1-preAlpha1.zip';
+            break;
+        case 4:
+            window.location.href='https://github.com/RedAnonymousITA/ip-tools/archive/refs/tags/v0.0.1.zip';
+            break;
+        case 5:
+            window.location.href='https://github.com/RedAnonymousITA/Generatore_Password/archive/refs/tags/v0.1-ALPHA.zip';
+            break;
+
+        case 6:
+            window.location.href='https://github.com/RedAnonymousITA/pyinstaller_GUI/archive/refs/tags/v0.1_Pre-Alpha.zip';
+            break;
+        case 7:
+            window.location.href='https://github.com/RedAnonymousITA/tarantella_sound_app/releases/download/V0.1_ALPHA.ko/tarantella_sound_app.apk';
+            break;
+        
+    }
+}
